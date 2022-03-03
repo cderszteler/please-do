f23d0434c07124bdf1e2f0097c8037764f38cd8b
Qetz <general@qetz.de>
2022-03-03 15:42:12 +0100

    Clean up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import React from 'react'
+import Head from 'next/head'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+function MyApp({Component, pageProps}: AppProps) {
+  return (
+    <React.Fragment>
+      <Head>
+        <title>Please do</title>
+        <link rel="icon" href="/favicon.svg"/>
+        <meta name="description" content="You can do stuff here"/>
+        <meta
+          name='viewport'
+          content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover'
+        />
+      </Head>
+      <Component {...pageProps} />
+    </React.Fragment>
+  )
 }
 
 export default MyApp
