4b6cb12c8b67e78c7bc80c17e7a216ebf7ecb5a3
Qetz <general@qetz.de>
2022-03-03 17:38:10 +0100

    Fix table design and add striped column

--- pages/components/Entry.tsx
+++ pages/components/Entry.tsx
@@ -2,12 +2,16 @@ import {Key} from "react";
 
 interface EntryProperties {
   key: Key
+  striped?: boolean
   children: JSX.Element
 }
 
 export function Entry(properties: EntryProperties) {
   return (
-    <tr key={properties.key}>
+    <tr
+      className={properties.striped ? "bg-gray-100" : ""}
+      key={properties.key}
+    >
       {properties.children}
     </tr>
   )
--- pages/components/todo/TodoEntry.tsx
+++ pages/components/todo/TodoEntry.tsx
@@ -4,16 +4,23 @@ import {Entry} from "../Entry";
 interface TodoEntryProperties {
   id: number,
   author: string,
-  children: string
+  children: string,
+  striped?: boolean
 }
 
 export default function TodoEntry(properties: TodoEntryProperties) {
   return (
-    <Entry key={properties.id}>
+    <Entry key={properties.id} striped={properties.striped}>
       <React.Fragment>
-        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">#{properties.id}</td>
-        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{properties.children}</td>
-        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{properties.author}</td>
+        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 font-semibold">
+          #{properties.id}
+        </td>
+        <td className="px-6 py-4 text-m text-gray-900">
+          {properties.children}
+        </td>
+        <td className="px-6 py-4 text-m text-gray-900">
+          {properties.author}
+        </td>
         <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
           <a href="#" className="text-indigo-600 hover:text-indigo-900">
             Edit
