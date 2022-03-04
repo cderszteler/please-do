546a24d36b3629c5ba0e4983b44aa1ca4f6cff9f
Qetz <general@qetz.de>
2022-03-04 21:36:12 +0100

    Add react hook

--- components/todo/TodoTable.tsx
+++ components/todo/TodoTable.tsx
@@ -1,12 +1,14 @@
 import {TodoProperties} from "../../pages/todo";
 import TodoEntry from "./TodoEntry";
 import Table from "../Table";
-import React from "react";
+import React, {useState} from "react";
 
 export default function TodoTable(properties: TodoProperties) {
+  const [todos, setTodos] = useState(properties.todos)
+
   return (
     <Table columns={["Id", "Task", "Author"]} actions={["Finish", "Delete"]}>
-      {properties.todos.map(todo => (
+      {todos.map(todo => (
         <TodoEntry id={todo.id} author={todo.author} key={todo.id}>
           {todo.task}
         </TodoEntry>
