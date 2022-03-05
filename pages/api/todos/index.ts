4be4e878651c6db7da94d1a17581d6079c0dbe40
Qetz <general@qetz.de>
2022-03-05 16:09:27 +0100

    Add state query to api

--- pages/api/todos/index.ts
+++ pages/api/todos/index.ts
@@ -4,17 +4,25 @@ import {errorCodes} from "../../../utils/errorCodes";
 import {prisma} from "../../../backend/prisma";
 import {authenticated} from "../../../utils/authenticated";
 
+async function queryTodos(state: string) {
+  const query = !state || state === ''
+    ? undefined
+    : {
+      where: {
+        state: state
+      }
+    }
+  return prisma.todos.findMany(query)
+}
+
 const list = authenticated(
   async (request: NextApiRequest, response: NextApiResponse) => {
     try {
-      const todos = await prisma.todos.findMany({
-        where: {
-          state: 'UNRESOLVED'
-        }
-      })
+      const state = request.query?.state as string
+      const todos = await queryTodos(state)
       response.status(200).json(todos)
     } catch (error) {
-      response.status(errorCodes.badRequest).end()
+      response.status(errorCodes.internal).json(error)
     }
   }
 )
