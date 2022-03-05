8d6027d252f3ed4b2f6431d77657a08c98faa730
Qetz <general@qetz.de>
2022-03-05 23:50:01 +0100

    Add top-padding only for certain device sizes

--- components/todo/TodoContainer.tsx
+++ components/todo/TodoContainer.tsx
@@ -7,7 +7,7 @@ interface TodoContainerProperties {
 export default function TodoContainer(properties: TodoContainerProperties) {
   return (
     <div className="flex">
-      <div className="flex flex-1 flex-col min-h-screen pt-8 items-center">
+      <div className="flex flex-1 flex-col min-h-screen sm:pt-8 items-center">
         {properties.children}
       </div>
     </div>
