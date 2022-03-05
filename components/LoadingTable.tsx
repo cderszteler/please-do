f2bc6a1b04fd8127e7d74c684871b071e54d36fd
Qetz <general@qetz.de>
2022-03-05 23:51:57 +0100

    Change top position of loading circle

--- components/LoadingTable.tsx
+++ components/LoadingTable.tsx
@@ -12,7 +12,7 @@ export default function LoadingTable(properties: LoadingTableProperties) {
       <div className="blur-[0.5px]">
         <Table columns={properties.columns} actions={[]}><></></Table>
       </div>
-      <div className="absolute top-0 z-10 w-full h-full flex justify-center">
+      <div className="absolute top-10 z-10 w-full h-full flex justify-center">
         <LoadingCircle attributes="z-100 w-16 h-16 text-gray-200 dark:text-gray-200 fill-gray-400 mt-20"/>
       </div>
     </React.Fragment>
